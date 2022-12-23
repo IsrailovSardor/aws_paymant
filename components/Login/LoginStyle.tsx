132d825c8b522bec92cc2c765a04343fa3a7fb3e
Israilov Sardor <Sardor_09062002@mail.ru>
2022-12-23 14:18:41 +0600

    bugFix

--- components/Login/LoginStyle.tsx
+++ components/Login/LoginStyle.tsx
@@ -7,6 +7,8 @@ export const WrapperLogin = styled.div`
     position: absolute;
     top: 0;
     left: 0;
+    bottom: 0;
+    right: 0;
     display: flex;
     align-items: center;
     justify-content: center;
